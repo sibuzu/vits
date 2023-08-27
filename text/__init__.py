ccf8ec7fe9d8585d0ba9080e6645bc674052ee89
Ming Shing Su <mingshing.su@gmail.com>
2023-08-27 10:00:22 +0800

    vits text symbol

--- text/__init__.py
+++ text/__init__.py
@@ -3,13 +3,13 @@ from text import cleaners
 # from text.symbols import symbols
 
 def create_symbol_id_map(symbols):
-    global _symbol_to_id, _id_to_symbol
     # Mappings from symbol to numeric ID and vice versa:
-    _symbol_to_id = {s: i for i, s in enumerate(symbols)}
-    _id_to_symbol = {i: s for i, s in enumerate(symbols)}
+    symbol_to_id = {s: i for i, s in enumerate(symbols)}
+    id_to_symbol = {i: s for i, s in enumerate(symbols)}
+    return symbol_to_id, id_to_symbol
 
 
-def text_to_sequence(text, cleaner_names):
+def text_to_sequence(text, cleaner_names, symbol_to_id):
   '''Converts a string of text to a sequence of IDs corresponding to the symbols in the text.
     Args:
       text: string to convert to a sequence
@@ -21,29 +21,29 @@ def text_to_sequence(text, cleaner_names):
 
   clean_text = _clean_text(text, cleaner_names)
   for symbol in clean_text:
-    if symbol not in _symbol_to_id.keys():
+    if symbol not in symbol_to_id.keys():
       continue
-    symbol_id = _symbol_to_id[symbol]
+    symbol_id = symbol_to_id[symbol]
     sequence += [symbol_id]
   return sequence
 
 
-def cleaned_text_to_sequence(cleaned_text):
+def cleaned_text_to_sequence(cleaned_text, symbol_to_id):
   '''Converts a string of text to a sequence of IDs corresponding to the symbols in the text.
     Args:
       text: string to convert to a sequence
     Returns:
       List of integers corresponding to the symbols in the text
   '''
-  sequence = [_symbol_to_id[symbol] for symbol in cleaned_text if symbol in _symbol_to_id.keys()]
+  sequence = [symbol_to_id[symbol] for symbol in cleaned_text if symbol in symbol_to_id.keys()]
   return sequence
 
 
-def sequence_to_text(sequence):
+def sequence_to_text(sequence, id_to_symbol):
   '''Converts a sequence of IDs back to a string'''
   result = ''
   for symbol_id in sequence:
-    s = _id_to_symbol[symbol_id]
+    s = id_to_symbol[symbol_id]
     result += s
   return result
 
